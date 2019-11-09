f5888a5265f00764a71de098a068fa0540a2a5bc
deepduke <runningwater.jc@gmail.com>
2019-11-09 11:18:21 +0800

    add 26-remove-duplicates-from-sorted-array.cpp

---
+++ Algorithms/C++/26-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,22 @@
+/*
+Method: two pointers, one points to the last unique element, 
+the other points to the current element.
+
+Time: O(n)
+*/
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        // special cases 
+        if(nums.size() < 2) return nums.size();
+        int cnt = 1;
+        for(int p = 0, q = 1; q < nums.size(); q++){
+            if(nums[p] == nums[q]) {
+                continue;
+            }
+            cnt++;
+            nums[++p] = nums[q];
+        }
+        return cnt;
+    }
+};
