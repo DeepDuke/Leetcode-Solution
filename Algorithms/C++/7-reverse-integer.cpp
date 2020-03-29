9442e8ff9b697faf911701a699a3c46e66c1724a
deepduke <runningwater.jc@gmail.com>
2020-03-29 21:13:06 +0800

    add 7-reverse-integer.cpp :strawberr

---
+++ Algorithms/C++/7-reverse-integer.cpp
@@ -0,0 +1,18 @@
+/*
+INT_MAX = 2147483647
+INT_MIN = -2147483648
+*/
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0, pop = 0;
+        while(x){
+            if(ans > INT_MAX/10 || ans == INT_MAX/10 && pop > 7) return 0;
+            if(ans < INT_MIN/10 || ans < INT_MIN/10 && pop < -8) return 0;
+            pop = x % 10;
+            ans = ans * 10 + pop;
+            x /= 10;
+        }
+        return ans;
+    }
+};
