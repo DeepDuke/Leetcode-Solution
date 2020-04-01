10aef367481f164958a6e04c6247b9be3144007a
deepduke <runningwater.jc@gmail.com>
2020-04-01 12:00:19 +0800

    :art: add another solution to 10-regular-expression-matching.cpp

--- Algorithms/C++/10-regular-expression-matching.cpp
+++ Algorithms/C++/10-regular-expression-matching.cpp
@@ -1,4 +1,4 @@
-// Solution 1
+// Solution 1 Top down
 class Solution {
 public:
     bool isMatch(string s, string p) {
@@ -20,4 +20,28 @@ public:
     }
 };
 
-// Solution 2
+// Solution 2 Bottom up
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        // dp method
+        vector<vector<bool> > dp(s.size()+1, vector<bool>(p.size()+1, 0));
+        dp[s.size()][p.size()] = true;  // matched
+        
+        for(int i = s.size(); i >= 0;  i--){
+            for(int j = p.size()-1; j >= 0; j--){
+                bool firstMatch = false;
+                if(i < s.size() && (p[j] == '.' || p[j] == s[i])){
+                    firstMatch = true;
+                }
+                if(j+1 < p.size() && p[j+1] == '*'){
+                    dp[i][j] = dp[i][j+2] || firstMatch && dp[i+1][j];
+                }else{
+                    dp[i][j] = firstMatch && dp[i+1][j+1];
+                }
+                //cout << "(" << i << ","  << j << "):\t" << dp[i][j] << endl; 
+            }
+        }
+        return dp[0][0];
+    }
+};
